b4484b199b3e755f66824199601ca5603af05697
Nikita Maximov <john.maks595@gmail.com>
2024-06-17 23:21:30 +0300

    Исправление для module.less

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './views/App/index';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./views/App/App";
 
-import './views/styles/reset.scss';
-import './views/styles/common.scss';
+import "./views/styles/reset.less";
+import "./views/styles/common.less";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
 if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+	const root = createRoot(container);
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,92 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  entry: "./src/main.tsx",
+	entry: "./src/main.tsx",
 
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js",
+	},
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".jsx"],
+	},
 
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.module\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                auto: true,
-                localIdentName: '[local]--[hash:base64:5]',
-                exportLocalsConvention: 'camelCaseOnly',
-              },
-            },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(scss|css)$/,
-        exclude: /\.module\.scss$/, 
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: devMode ? "[name].css" : "[name].[contenthash].css",
+			chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+		}),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.less$/,
+				oneOf: [
+					{
+						test: /\.module\.less$/,
+						use: [
+							devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+							{
+								loader: "css-loader",
+								options: {
+									modules: {
+										localIdentName: "[local]--[hash:base64:5]",
+										exportLocalsConvention: "camelCaseOnly",
+									},
+									importLoaders: 2,
+									esModule: false,
+								},
+							},
+							"less-loader",
+						],
+					},
+					{
+						use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+					},
+				],
+			},
+		],
+	},
+
+	optimization: {
+		minimize: !devMode,
+		minimizer: [
+			new TerserPlugin({
+				parallel: true,
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
+
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		compress: true,
+		port: 9000,
+		hot: true,
+	},
 };
